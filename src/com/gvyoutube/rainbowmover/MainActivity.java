e1f4af9c24466f4dbef75ac6274dc441b56fb24b
GvYoutube <119984713+Gvyt@users.noreply.github.com>
2025-06-25 14:30:16 -0500

    Update MainActivity.java

--- src/com/gvyoutube/rainbowmover/MainActivity.java
+++ src/com/gvyoutube/rainbowmover/MainActivity.java
@@ -1,15 +1,16 @@
 package com.gvyoutube.rainbowmover;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.VideoView;
-import android.widget.MediaController;
 import android.net.Uri;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.MediaController;
 import android.widget.ProgressBar;
+import android.widget.VideoView;
+import android.media.MediaPlayer;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private VideoView videoView;
     private ProgressBar progressBar;
@@ -20,8 +21,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        videoView = findViewById(R.id.videoView);
-        progressBar = findViewById(R.id.progressBar);
+        videoView = (VideoView) findViewById(R.id.videoView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         Uri videoUri = Uri.parse("https://file.garden/Z4ry-cpnZQh7VmhL/Rainbow%20Trololol.mp4");
         videoView.setVideoURI(videoUri);
@@ -32,26 +33,32 @@ public class MainActivity extends Activity {
 
         progressBar.setVisibility(View.VISIBLE);
 
-        videoView.setOnPreparedListener(mp -> {
-            progressBar.setVisibility(View.GONE);
-            videoView.start();
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                progressBar.setVisibility(View.GONE);
+                videoView.start();
+            }
         });
 
-        videoView.setOnTouchListener((v, event) -> {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    dX = v.getX() - event.getRawX();
-                    dY = v.getY() - event.getRawY();
-                    return true;
-                case MotionEvent.ACTION_MOVE:
-                    v.animate()
-                        .x(event.getRawX() + dX)
-                        .y(event.getRawY() + dY)
-                        .setDuration(0)
-                        .start();
-                    return true;
-                default:
-                    return false;
+        videoView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        dX = v.getX() - event.getRawX();
+                        dY = v.getY() - event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        v.animate()
+                            .x(event.getRawX() + dX)
+                            .y(event.getRawY() + dY)
+                            .setDuration(0)
+                            .start();
+                        return true;
+                    default:
+                        return false;
+                }
             }
         });
     }
