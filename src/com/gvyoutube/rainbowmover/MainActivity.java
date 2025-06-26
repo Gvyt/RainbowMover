10a4f496709c6cffa79d11a1ce779b9e9a8e91dd
GvYoutube <119984713+Gvyt@users.noreply.github.com>
2025-06-25 20:21:08 -0500

    Update MainActivity.java

--- src/com/gvyoutube/rainbowmover/MainActivity.java
+++ src/com/gvyoutube/rainbowmover/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.VideoView;
 import android.widget.ProgressBar;
 import android.widget.MediaController;
+import android.media.MediaPlayer;
 import android.view.View;
 import android.net.Uri;
 import android.widget.Toast;
@@ -14,6 +15,8 @@ public class MainActivity extends Activity {
     private VideoView videoView;
     private ProgressBar progressBar;
 
+    private final String videoUrl = "https://ia601508.us.archive.org/16/items/rainbow-trololol/Rainbow%20Trololol.mp4";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,24 +25,30 @@ public class MainActivity extends Activity {
         videoView = findViewById(R.id.videoView);
         progressBar = findViewById(R.id.progressBar);
 
-        String videoUrl = "https://ia601508.us.archive.org/16/items/rainbow-trololol/Rainbow%20Trololol.mp4";
-        videoView.setVideoURI(Uri.parse(videoUrl));
+        progressBar.setVisibility(View.VISIBLE);
+
+        Uri uri = Uri.parse(videoUrl);
+        videoView.setVideoURI(uri);
 
         MediaController mediaController = new MediaController(this);
-        videoView.setMediaController(mediaController);
         mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
 
-        videoView.setOnPreparedListener(mp -> {
-            progressBar.setVisibility(View.GONE);
-            videoView.start();
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                progressBar.setVisibility(View.GONE);
+                videoView.start();
+            }
         });
 
-        videoView.setOnErrorListener((mp, what, extra) -> {
-            progressBar.setVisibility(View.GONE);
-            Toast.makeText(this, "Unable to play the video.", Toast.LENGTH_LONG).show();
-            return true;
+        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(MainActivity.this, "Unable to play the video.", Toast.LENGTH_LONG).show();
+                return true;
+            }
         });
-
-        progressBar.setVisibility(View.VISIBLE);
     }
 }
