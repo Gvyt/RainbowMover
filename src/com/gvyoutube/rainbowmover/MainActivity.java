1e8c7d2d62bbfca9da10139b563665fca20feb3c
GvYoutube <119984713+Gvyt@users.noreply.github.com>
2025-06-26 15:32:27 -0500

    Update MainActivity.java

--- src/com/gvyoutube/rainbowmover/MainActivity.java
+++ src/com/gvyoutube/rainbowmover/MainActivity.java
@@ -1,6 +1,7 @@
 package com.gvyoutube.rainbowmover;
 
 import android.app.Activity;
+import android.widget.CompoundButton;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
