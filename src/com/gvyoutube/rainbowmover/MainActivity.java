563c7a145f1b23d59334a8f6d7bb5d687eae7c4d
GvYoutube <119984713+Gvyt@users.noreply.github.com>
2025-06-25 19:51:59 -0500

    Update MainActivity.java

--- src/com/gvyoutube/rainbowmover/MainActivity.java
+++ src/com/gvyoutube/rainbowmover/MainActivity.java
@@ -1,15 +1,15 @@
 package com.gvyoutube.rainbowmover;
 
 import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.ProgressBar;
 import android.widget.VideoView;
+import android.widget.ProgressBar;
+import android.net.Uri;
 import android.widget.MediaController;
 import android.media.MediaPlayer;
+import android.view.View;
 
 public class MainActivity extends Activity {
-
     private VideoView videoView;
     private ProgressBar progressBar;
 
@@ -21,18 +21,17 @@ public class MainActivity extends Activity {
         videoView = findViewById(R.id.videoView);
         progressBar = findViewById(R.id.progressBar);
 
-        MediaController mediaController = new MediaController(this);
-        videoView.setMediaController(mediaController);
-        mediaController.setAnchorView(videoView);
-
         String videoUrl = "https://file.garden/Z4ry-cpnZQh7VmhL/Rainbow%20Trololol.mp4";
         Uri videoUri = Uri.parse(videoUrl);
+
         videoView.setVideoURI(videoUri);
+        videoView.setMediaController(new MediaController(this));
+        progressBar.setVisibility(View.VISIBLE);
 
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
-                progressBar.setVisibility(ProgressBar.GONE);
+                progressBar.setVisibility(View.GONE);
                 videoView.start();
             }
         });
@@ -40,8 +39,8 @@ public class MainActivity extends Activity {
         videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
             @Override
             public boolean onError(MediaPlayer mp, int what, int extra) {
-                progressBar.setVisibility(ProgressBar.GONE);
-                return true;
+                progressBar.setVisibility(View.GONE);
+                return false;
             }
         });
     }
