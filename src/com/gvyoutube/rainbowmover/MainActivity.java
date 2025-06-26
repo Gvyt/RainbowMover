68f4f4327100e1c6f9eefe7f9147cc3f11b1f72a
GvYoutube <119984713+Gvyt@users.noreply.github.com>
2025-06-25 20:33:53 -0500

    Update MainActivity.java

--- src/com/gvyoutube/rainbowmover/MainActivity.java
+++ src/com/gvyoutube/rainbowmover/MainActivity.java
@@ -1,16 +1,17 @@
 package com.gvyoutube.rainbowmover;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.widget.VideoView;
-import android.widget.ProgressBar;
 import android.widget.MediaController;
-import android.media.MediaPlayer;
-import android.net.Uri;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+import android.widget.VideoView;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
@@ -18,73 +19,88 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends Activity {
+
     private VideoView videoView;
     private ProgressBar progressBar;
-
-    private static final String VIDEO_URL = "https://ia601508.us.archive.org/16/items/rainbow-trololol/Rainbow%20Trololol.mp4";
-    private static final String LOCAL_FILE_NAME = "rainbow_trololol.mp4";
+    private static final String videoUrl = "https://ia601508.us.archive.org/16/items/rainbow-trololol/Rainbow%20Trololol.mp4";
+    private static final String localFileName = "appvid.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        videoView = findViewById(R.id.videoView);
-        progressBar = findViewById(R.id.progressBar);
+        videoView = (VideoView) findViewById(R.id.videoView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        File localVideo = new File(getFilesDir(), LOCAL_FILE_NAME);
-
-        if (localVideo.exists()) {
-            playVideo(localVideo);
-        } else {
-            progressBar.setVisibility(ProgressBar.VISIBLE);
-            downloadVideo(VIDEO_URL, localVideo);
-        }
+        downloadVideo(videoUrl, new File(getFilesDir(), localFileName));
     }
 
-    private void downloadVideo(String urlString, File outputFile) {
-        new Thread(() -> {
-            try {
-                URL url = new URL(urlString);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.connect();
+    private void downloadVideo(final String urlString, final File outputFile) {
+        progressBar.setVisibility(ProgressBar.VISIBLE);
 
-                InputStream in = conn.getInputStream();
-                FileOutputStream out = new FileOutputStream(outputFile);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(urlString);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.connect();
 
-                byte[] buffer = new byte[4096];
-                int len;
-                while ((len = in.read(buffer)) > 0) {
-                    out.write(buffer, 0, len);
+                    InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                    FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+
+                    byte[] buffer = new byte[1024];
+                    int len;
+                    while ((len = inputStream.read(buffer)) != -1) {
+                        fileOutputStream.write(buffer, 0, len);
+                    }
+
+                    fileOutputStream.close();
+                    inputStream.close();
+
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setVisibility(ProgressBar.GONE);
+                            playVideo(outputFile);
+                        }
+                    });
+                } catch (final Exception e) {
+                    e.printStackTrace();
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setVisibility(ProgressBar.GONE);
+                            Toast.makeText(MainActivity.this, "Failed to download video: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
                 }
-
-                in.close();
-                out.close();
-
-                new Handler(Looper.getMainLooper()).post(() -> {
-                    progressBar.setVisibility(ProgressBar.GONE);
-                    playVideo(outputFile);
-                });
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                new Handler(Looper.getMainLooper()).post(() -> {
-                    progressBar.setVisibility(ProgressBar.GONE);
-                    Toast.makeText(this, "Download failed: " + e.getMessage(), Toast.LENGTH_LONG).show();
-                });
             }
         }).start();
     }
 
-    private void playVideo(File videoFile) {
-        Uri videoUri = Uri.fromFile(videoFile);
+    private void playVideo(File file) {
+        Uri videoUri = Uri.fromFile(file);
+
         videoView.setVideoURI(videoUri);
         MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
         videoView.setMediaController(mediaController);
-        videoView.setOnPreparedListener(mp -> videoView.start());
-        videoView.setOnErrorListener((mp, what, extra) -> {
-            Toast.makeText(this, "Can't play the downloaded video.", Toast.LENGTH_LONG).show();
-            return true;
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoView.start();
+            }
+        });
+
+        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Toast.makeText(MainActivity.this, "Can't play the downloaded video.", Toast.LENGTH_LONG).show();
+                return true;
+            }
         });
     }
 }
